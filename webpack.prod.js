5d2e35bbf46bdf1527c224b244091fe809a38b9a
Rebecca Dai <rebecca.dai@liferay.com>
2018-05-15 16:27:04 -0700

    LRIS-31359 Only uglify for production environment

---
+++ webpack.prod.js
@@ -0,0 +1,20 @@
+const common = require('./webpack.common.js');
+const merge = require('webpack-merge');
+
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = merge(common, {
+  optimization: {
+		minimizer: [
+			new OptimizeCssAssetsPlugin(
+				{
+					cssProcessorOptions: {
+						discardComments: {removeAll: true}
+					}
+				}
+			),
+			new UglifyJsPlugin()
+		]
+	}
+});
