c2acb8c0001c993c1be5782afcca5f8f5ed62658
Rebecca Dai <rebecca.dai@liferay.com>
2018-07-10 17:13:18 -0700

    LRIS-31779 list of articles shouldn't share the same state & format source

--- src/js/components/documentation-side-nav.js
+++ src/js/components/documentation-side-nav.js
@@ -2,45 +2,73 @@ import preact from 'preact';
 
 import * as apiHelpers from '../helpers/api-helpers.js';
 
-class DisplayArticles extends preact.Component {
+class ArticlesList extends preact.Component {
 	constructor(props) {
 		super(props);
 
+		this.handleClick = this.handleClick.bind(this);
+
 		this.state = {
-			loading: true,
-			items: []
+			expanded: false,
+			items: [],
+			loading: false
 		}
 	}
 
-	componentDidMount() {
-		apiHelpers.getArticlesBySectionId(this.props.locale, this.props.id)
-		.then(
-			({data}) => {this.setState({
-				items: data,
-				loading: false
+	getArticles() {
+		const {locale, id} = this.props;
+
+		this.setState({loading: true});
+
+		apiHelpers.getArticlesBySectionId(locale, id)
+			.then(({data}) => {
+				this.setState({
+					items: data,
+					loading: false
+				});
 			})
-		})
-		.catch(err => this.setState({loading: true}));
+			.catch(err => this.setState({loading: false}));
 	}
 
-	render() {
-		const {items, loading} = this.state;
+	handleClick() {
+		this.setState({expanded: !this.state.expanded});
 
-		if (loading) {
-			return <span aria-hidden="true" class="loading-animation loading-animation-sm"></span>
+		if (!this.state.items.length) {
+			this.getArticles();
 		}
-		else {
-			return (
-				<ul class="nav nav-nested">
-					{items.articles.map(item => (
-						<li class="nav-item" key={item.id}>
-							<a class="sidenav-item" href={item.html_url}>{item.name}</a>
-						</li>
-						)
-					)}
-				</ul>
-			);
-		}
+	}
+
+	render({name}, {expanded, items, loading}) {
+		return (
+			<div>
+				<a
+					class="sidenav-item"
+					href="javascript:;"
+					onClick={this.handleClick}
+				>
+					{name}
+				</a>
+
+				{expanded && !loading &&
+					<ul class="nav nav-nested">
+						{items.articles.map(item => (
+							<li class="nav-item" key={item.id}>
+								<a class="sidenav-item" href={item.html_url}>{item.name}</a>
+							</li>
+							)
+						)}
+					</ul>
+				}
+
+				{loading &&
+					<span
+						aria-hidden="true"
+						class="loading-animation loading-animation-sm"
+					/>
+				}
+
+			</div>
+		);
 	}
 }
 
@@ -49,64 +77,62 @@ export default class DocSideNav extends preact.Component {
 		super(props);
 
 		this.state = {
-			active: false,
-			error: false,
-			loading: true,
 			items: [],
-			showArticles: false
+			loading: true
 		};
 	}
 
 	componentDidMount() {
-		apiHelpers.getSectionsCategories(this.props.locale)
-		.then(
-			({data}) => {
-			let productDocCategory = data.categories.find(item => item.name === "Product Documentation");
+		const {locale} = this.props;
 
-			if (productDocCategory.id) {
-				let productDocSections = data.sections.filter(item => item.category_id === productDocCategory.id);
+		apiHelpers.getSectionsCategories(locale)
+			.then(
+				({data}) => {
+					let productDocCategory = data.categories.find(
+						item => item.name === "Analytics Cloud Documentation"
+					);
 
-				this.setState({items: productDocSections});
+					if (productDocCategory.id) {
+						let productDocSections = data.sections.filter(
+							item => item.category_id === productDocCategory.id
+						);
+
+						this.setState({
+							items: productDocSections,
+							loading: false
+						});
+					}
+				}
+			)
+			.catch(err => {
+				const sidenavFallback = document.getElementById('sidenavFallback');
+
+				this.setState({loading: false});
+
+				sidenavFallback.classList.add('show');
 			}
-		})
-		.catch(err => {
-			this.setState({error: true});
-		});
-
-		this.setState({loading: false});
+		);
 	}
 
-	handleClick() {
-		this.setState({
-			active: true,
-			showArticles: !this.state.showArticles
-		});
-	}
-
-	render() {
-		const {error, items, loading} = this.state;
-
-		if (error) {
-			const sidenavFallback = document.getElementById('sidenavFallback');
-
-			sidenavFallback.classList.add('show');
-		}
-		else if (loading) {
-			return <span aria-hidden="true" class="loading-animation loading-animation-sm"></span>
+	render({locale}, {items, loading}) {
+		if (loading) {
+			return (
+				<span
+					aria-hidden="true"
+					class="loading-animation loading-animation-sm"
+				/>
+			);
 		}
 		else {
-			let className = this.state.active ? 'active nav-item': 'nav-item';
-
 			return (
 				<ul class="nav nav-nested">
 					{items.map(item => (
-						<li class={className} key={item.id}>
-							<a class="sidenav-item" href="javascript:;" onClick={this.handleClick.bind(this)}>{item.name}</a>
-
-							{this.state.showArticles ?
-								<DisplayArticles id={item.id} locale={this.props.locale} /> :
-								null
-							}
+						<li class="nav-item" key={item.id}>
+							<ArticlesList
+								id={item.id}
+								locale={locale}
+								name={item.name}
+							/>
 						</li>
 						)
 					)}
