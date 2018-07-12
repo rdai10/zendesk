d8524ceed31ca9ced34edd1de31f538c1fb37ff6
Rebecca Dai <rebecca.dai@liferay.com>
2018-07-12 15:49:36 -0700

    LRIS-32059 specify system environment variable

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,6 @@
 const common = require('./webpack.common.js');
 const merge = require('webpack-merge');
+const webpack = require('webpack');
 
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
@@ -16,5 +17,10 @@ module.exports = merge(common, {
 			),
 			new UglifyJsPlugin()
 		]
-	}
+	},
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify('production')
+		})
+	]
 });
