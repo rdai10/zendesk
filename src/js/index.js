4a6bab14139750afa746de67abbbec1f0177095b
Rebecca Dai <rebecca.dai@liferay.com>
2018-07-24 15:06:27 -0700

    LRIS-32236 add boxshadow to header on scroll

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@ import './components/DocSideNav';
 import './vendor/zendesk-script';
 
 export {default as render} from './helpers/preact-renderer';
+export {default as throttle} from 'lodash.throttle';
 
 export {default as DocSideNav} from './components/DocSideNav';
 export {default as MegaMenu} from './components/MegaMenu';
