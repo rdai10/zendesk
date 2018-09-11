084275bdd56dea4c38b2d9e238a61ed027e3c579
Rebecca Dai <rhdai10@gmail.com>
2018-09-11 11:31:01 -0700

    LRIS-32568 add alert component

---
+++ theme/src/js/components/Alert.js
@@ -0,0 +1,22 @@
+import preact from 'preact';
+import PropTypes from 'prop-types';
+
+const Alert = ({children, leadingText}) => (
+	<div class="alert alert-info" role="alert">
+		<span class="alert-indicator">
+			<svg className="lexicon-icon lexicon-icon-info-circle">
+				<use xlinkHref="#info-circle" />
+			</svg>
+		</span>
+
+		<strong class="lead">{leadingText}</strong>
+		{children}
+	</div>
+);
+
+Alert.propTypes = {
+	children: PropTypes.node.isRequired,
+	leadingText: PropTypes.string
+};
+
+export default Alert;
