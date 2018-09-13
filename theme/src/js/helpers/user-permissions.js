c3502387a3353b0ec798cd9beaa547a46d76ce0f
Rebecca Dai <rhdai10@gmail.com>
2018-09-12 18:08:12 -0700

    LRIS-32774 only show large file attachment for non watchers

--- theme/src/js/helpers/user-permissions.js
+++ theme/src/js/helpers/user-permissions.js
@@ -1,5 +1,24 @@
-export default function hasCreateTicketPermission(userTags) {
+import {getRequestById} from './api-helpers';
+
+export function hasCreateTicketPermission(userTags) {
 	return (
-		userTags && userTags.some(tag => tag === 'osb_customer' || tag === 'osb_partner')
+		userTags &&
+		userTags.some(tag => tag === 'osb_customer' || tag === 'osb_partner')
 	);
+}
+
+export function hasWatcherPermission(userTags, id, callback) {
+	getRequestById(id)
+		.then(({data}) => {
+			const orgWatcherArray = userTags.filter(tag =>
+				tag.match(data.request.organization_id)
+			);
+
+			if (!!!orgWatcherArray.length) {
+				callback();
+			}
+		})
+		.catch(err => {
+			process.env.NODE_ENV === 'production' && console.log(err);
+		});
 }
