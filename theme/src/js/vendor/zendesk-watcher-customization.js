2d29ce15084ba6df92e3f6333af734f2b0ad3f06
Rebecca Dai <rhdai10@gmail.com>
2018-09-17 12:27:32 -0700

    LRIS-33021 add in new customization code from Zendesk consulting

--- theme/src/js/vendor/zendesk-watcher-customization.js
+++ theme/src/js/vendor/zendesk-watcher-customization.js
@@ -1,168 +1,143 @@
-$(document).ready(
-	function() {
-		if (location.pathname.indexOf('/requests/new') > -1) {
-			getDevOrgs();
-
-			var allowedOrgsByName = HelpCenter.user.allowedOrgsByName;
-			var allowedUserOrgs = HelpCenter.user.allowedOrgs;
-
-			var form = $('div.watcher-message').next('div');
-
-			var key = Object.keys(allowedUserOrgs)[0];
-			var value = Object.values(allowedUserOrgs)[0];
-
-			if (value) {
-				if (!allowedOrgsByName[$('.request_organization_id a.nesty-input').text()]) {
-					$('.request_organization_id a.nesty-input').text(value);
-
-					$('#request_organization_id option').each(
-						function(option) {
-							if ($(this).val() == key) {
-								$(this).attr('selected', true);
-							}
-							else {
-								$(this).attr('selected', false);
-							}
-						}
-					);
-				}
-
-				$('.watcher-message').remove();
-
-				$(form).removeClass('d-none');
-			}
-			else {
-				$(form).remove();
-
-				$('.watcher-message').removeClass('d-none');
-			}
-
-			$('.request_organization_id a.nesty-input').on(
-				'click',
-				function() {
-					$('#request_organization_id option').each(
-						function(option) {
-							var id = $(this).val();
-
-							if (!allowedUserOrgs[id]) {
-								$('#' + id).remove();
-							}
-						}
-					);
-				}
-			);
-		}
-		else if (location.pathname.match(/\/requests\/\d+/)) {
-			getRequestOrg();
-		}
+$(document).ready(function() {
+	if (
+		location.pathname.indexOf('/requests/new') > -1 ||
+		location.pathname.match(/\/requests$/)
+	) {
+		getDevOrgs();
+	} else if (location.pathname.match(/\/requests\/\d+/)) {
+		getRequestOrg();
 	}
-);
+});
 
 Array.prototype.subtract = function(arr) {
-	return this.filter(
-		function(el) {
-			return arr.indexOf(el) === -1;
-		}
-	);
+	return this.filter(function(el) {
+		return arr.indexOf(el) === -1;
+	});
 };
 
-// filter user tags to obtain IDs of tags with the osb_[id]_watcher suffix
-
+// filter user tags to obtain IDs of tags with the _watcher suffix
 function getUserWatcherTags(userTags) {
-	return userTags.filter(
-		function(tag) {
-			return tag.match(/osb_(\d*)_watcher/);
-		}
-	)
-	.map(
-		function(tag) {
-			return tag.split('_')[1];
-		}
-	);
+	return userTags
+		.filter(function(tag) {
+			return tag.match(/(\d*)_watcher/);
+		})
+		.map(function(tag) {
+			return tag.split('_')[0];
+		});
 }
 
 // obtain dev orgs for current user
-
 function getDevOrgs() {
-	if (!HelpCenter.user.allowedOrgs) {
-		var orgs = {};
-
-		$('#request_organization_id option').each(
-			function(option) {
-				orgs[$(this).val()] = $(this).text();
-			}
-		);
-
-		var allOrgs = Object.keys(orgs);
-
-		var userTags = window.HelpCenter.user.tags;
-
-		var watcherOrgs = getUserWatcherTags(userTags);
-
-		var allowedOrgs = allOrgs.subtract(watcherOrgs);
-
-		var allowedUserOrgs = {};
-
-		allowedOrgs.forEach(
-			function(id) {
-				if (orgs[id]) {
-					allowedUserOrgs[id] = orgs[id];
+	var orgs = {};
+	var orgOptions = $('#request_organization_id option');
+	if ($(orgOptions).length > 0) {
+		$(orgOptions).each(function(option) {
+			orgs[$(this).val()] = $(this).text();
+		});
+		assignOrgs(orgs);
+	} else if (HelpCenter.user.organizations.length > 0) {
+		$.ajax('/api/v2/users/me')
+			.done(function(data) {
+				if (data.user) {
+					orgs[data.user.organization_id] =
+						HelpCenter.user.organizations[0].name;
 				}
-			}
-		);
-
-		HelpCenter.user['allowedOrgs'] = allowedUserOrgs;
-		HelpCenter.user['allowedOrgsByName'] = {};
-
-		Object.keys(allowedUserOrgs).forEach(
-			function(key) {
-				HelpCenter.user['allowedOrgsByName'][allowedUserOrgs[key]] = key;
-			}
-		);
+				assignOrgs(orgs);
+			})
+			.fail(function(error) {
+				console.log(error);
+			});
+	} else {
+		showWatcherMessage();
 	}
 }
 
-// remove comment box in request page
+function assignOrgs(orgs) {
+	var allOrgs = Object.keys(orgs);
+	var userTags = HelpCenter.user.tags;
+	var watcherOrgs = getUserWatcherTags(userTags);
+	var allowedOrgs = allOrgs.subtract(watcherOrgs);
 
-function disableComments() {
-	$('.comment-form').remove();
+	var allowedOrgsById = {};
+	var allowedOrgsByName = {};
+
+	allowedOrgs.forEach(function(id) {
+		allowedOrgsById[id] = orgs[id];
+		allowedOrgsByName[orgs[id]] = id;
+	});
+
+	var key = Object.keys(allowedOrgsByName)[0];
+	var value = Object.values(allowedOrgsByName)[0];
+	if (value) {
+		// if the default org is a watched org, select the next dev to be default
+		if (
+			!allowedOrgsByName[
+				$('.request_organization_id a.nesty-input').text()
+			]
+		) {
+			$('.request_organization_id a.nesty-input').text(key);
+			$('#request_organization_id option').each(function(option) {
+				if ($(this).val() === key) {
+					$(this).attr('selected', true);
+				} else {
+					$(this).attr('selected', false);
+				}
+			});
+		}
+		showForm();
+	} else {
+		showWatcherMessage();
+	}
+	$('.request_organization_id a.nesty-input').on('click', function() {
+		var option = this;
+		$('#request_organization_id option').each(function(option) {
+			var id = $(this).val();
+			if (!allowedOrgsById[id]) {
+				$('#' + id).remove();
+			}
+		});
+	});
+}
+
+function showWatcherMessage() {
+	$('.watcher-message')
+		.next('div')
+		.remove();
+	$('.watcher-message').removeClass('hide');
+}
+
+function showForm() {
+	$('.watcher-message')
+		.next('div')
+		.removeClass('hide');
+	$('.watcher-message').remove();
 }
 
 // get org of current request
-
 function getRequestOrg() {
 	var id = location.pathname.match(/\/requests\/(\d+)/)[1];
 
 	$.ajax('/api/v2/requests/' + id)
-	.done(
-		function(data) {
+		.done(function(data) {
 			if (data.request) {
 				var requestOrgId = data.request.organization_id;
-
 				customizeRequestPage(requestOrgId);
 			}
-		}
-	)
-	.fail(
-		function(error) {
+		})
+		.fail(function(error) {
 			console.log(error);
-		}
-	);
+		});
 }
 
 // on request page, disable org drop-down and remove comment box if necessary
-
 function customizeRequestPage(id) {
 	$('#request_organization_id').attr('disabled', true);
-
-	var userTags = window.HelpCenter.user.tags;
-
+	var userTags = HelpCenter.user.tags;
 	var watcherOrgs = getUserWatcherTags(userTags);
 
 	if (watcherOrgs.indexOf(String(id)) > -1) {
-		disableComments();
-	}
-	else {
+		$('.comment-form').remove();
+	} else {
 		$('.request-container .comment-form').css('display', 'flex');
 	}
 }
