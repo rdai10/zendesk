432e675cf38ad3666811d61c845081df5a37e023
Jonathan Mak <jonathan.mak@liferay.com>
2018-12-17 17:31:17 -0800

    LRIS-32949 - Source formatting

--- theme/src/js/helpers/article-accordion.js
+++ theme/src/js/helpers/article-accordion.js
@@ -1,12 +1,10 @@
 $(function() {
-	const articlePanels = document.querySelectorAll(
-		'.article-body .panel'
-	);
+	const articlePanels = document.querySelectorAll('.article-body .panel');
 
 	articlePanels.forEach(
 		panel => {
 			panel.addEventListener(
-				'click', 
+				'click',
 				(event) => {
 					if (!event.target.classList.contains('panel-body')) {
 						event.currentTarget.classList.toggle('show');
