ba0fe9bfcc3f788de33fa1a6cf08c427e6788df5
Rebecca <rhdai10@gmail.com>
2021-08-24 15:46:31 -0700

    ISSUP-4247 add search filter

--- knowledge-capture-app/src/javascript/modules/SearchResults.js
+++ knowledge-capture-app/src/javascript/modules/SearchResults.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Col, Grid, Row } from '@zendeskgarden/react-grid';
 import { Well, Title, Paragraph } from '@zendeskgarden/react-notifications';
 import { Ellipsis, MD, SM } from '@zendeskgarden/react-typography';
 
@@ -6,26 +7,36 @@ import I18n from '../lib/i18n';
 import { displayDateInMDYFormat } from '../lib/utility';
 
 export default function SearchResults({ results }) {
-	return results.map((result) => <Result key={result.id} result={result} />);
+	return (
+		<Grid>
+			<Row>
+				{results.map((result) => (
+					<Result key={result.id} result={result} />
+				))}
+			</Row>
+		</Grid>
+	);
 }
 
 function Result({ result }) {
 	return (
-		<Well>
-			<Title>
-				<MD isBold>{result.name}</MD>
-			</Title>
+		<Col>
+			<Well>
+				<Title>
+					<MD isBold>{result.name}</MD>
+				</Title>
 
-			<SM>
-				<Ellipsis>
-					{/* <Paragraph size="small">breadcrumb</Paragraph> */}
-				</Ellipsis>
+				<SM>
+					<Ellipsis>
+						{/* <Paragraph size="small">breadcrumb</Paragraph> */}
+					</Ellipsis>
 
-				<Paragraph size="small">
-					{I18n.t('last edited')}{' '}
-					{displayDateInMDYFormat(result.edited_at)}
-				</Paragraph>
-			</SM>
-		</Well>
+					<Paragraph size="small">
+						{I18n.t('last edited')}{' '}
+						{displayDateInMDYFormat(result.edited_at)}
+					</Paragraph>
+				</SM>
+			</Well>
+		</Col>
 	);
 }
