550aae6a9963d69c61ee902fa0cd5845be425677
Rebecca <rhdai10@gmail.com>
2021-08-25 18:45:07 -0700

    ISSUP-4243 add ability to link search results to ticket editor

--- knowledge-capture-app/src/javascript/modules/SearchResults.js
+++ knowledge-capture-app/src/javascript/modules/SearchResults.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button } from '@zendeskgarden/react-buttons';
 import { Col, Grid, Row } from '@zendeskgarden/react-grid';
 import { Well, Title, Paragraph } from '@zendeskgarden/react-notifications';
+import { Tag } from '@zendeskgarden/react-tags';
 import { Ellipsis, MD, SM } from '@zendeskgarden/react-typography';
-
+import { useGlobalContext } from '../context/Global';
 import I18n from '../lib/i18n';
 import { displayDateInMDYFormat } from '../lib/utility';
 
@@ -17,6 +19,19 @@ export default function SearchResults({ results }) {
 }
 
 function Result({ result }) {
+	const { client } = useGlobalContext();
+
+	const [linked, setLinked] = useState(false);
+
+	function handleClick() {
+		client.invoke(
+			'ticket.editor.insert',
+			`<a href=${result.html_url} rel="noopener noreferrer" target="_blank" >${result.name}</a>`
+		);
+
+		setLinked(true);
+	}
+
 	return (
 		<Row>
 			<Col>
@@ -35,6 +50,22 @@ function Result({ result }) {
 							{displayDateInMDYFormat(result.edited_at)}
 						</Paragraph>
 					</SM>
+
+					<Row alignItems="center">
+						<Col>
+							<Button isLink size="small" onClick={handleClick}>
+								{I18n.t('link article')}
+							</Button>
+						</Col>
+
+						{linked && (
+							<Col textAlign="end">
+								<Tag hue="blue">
+									<span>{I18n.t('linked')}</span>
+								</Tag>
+							</Col>
+						)}
+					</Row>
 				</Well>
 			</Col>
 		</Row>
