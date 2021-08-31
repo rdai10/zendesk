1a95f9da7d1c51a47fc2ad3e43ca6ab001663b85
Rebecca <rhdai10@gmail.com>
2021-08-31 09:17:05 -0700

    ISSUP-4242 allow keyword search - enter to submit new search keyword

--- knowledge-capture-app/src/javascript/modules/SearchInput.js
+++ knowledge-capture-app/src/javascript/modules/SearchInput.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import { Field, Label, Input } from '@zendeskgarden/react-forms';
+import React, { useState } from 'react';
+import { Field, Label, MediaInput } from '@zendeskgarden/react-forms';
 import { Col, Grid, Row } from '@zendeskgarden/react-grid';
 
 import i18n from '../lib/i18n';
 
-export default function SearchInput({ placeholder, updateKeyword }) {
+export default function SearchInput({ updateKeyword, value }) {
+	const [currentValue, setCurrentValue] = useState(value);
+
+	function handleOnChange(event) {
+		setCurrentValue(event.currentTarget.value);
+	}
+
+	function handleOnKeyDown(event) {
+		if (event.keyCode === 13 && updateKeyword) {
+			updateKeyword(currentValue);
+		}
+	}
+
 	return (
 		<Grid>
 			<Row>
 				<Col>
 					<Field>
 						<Label hidden>{i18n.t('search input')}</Label>
-						<Input placeholder={placeholder} />
+						<MediaInput
+							onChange={handleOnChange}
+							onKeyDown={handleOnKeyDown}
+							value={currentValue}
+						/>
 					</Field>
 				</Col>
 			</Row>
