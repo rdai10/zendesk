ba6b73252933027434fcdf637f518405673b466e
Rebecca <rhdai10@gmail.com>
2021-09-02 16:55:32 -0700

    ISSUP-4244 add tests to assert the correct workflow and fix existing tests

---
+++ knowledge-capture-app/spec/Main.spec.js
@@ -0,0 +1,59 @@
+import { cleanup, fireEvent, getByText, render } from '@testing-library/react';
+import React from 'react';
+import { ThemeProvider } from '@zendeskgarden/react-theming';
+import { CLIENT } from './mocks/mock';
+import { GlobalContext } from '../src/javascript/context/Global';
+import Main from '../src/javascript/modules/Main';
+import { Theme } from '../src/javascript/modules/Theme';
+
+function renderMain() {
+	return render(
+		<GlobalContext.Provider value={{ client: CLIENT }}>
+			<ThemeProvider theme={Theme}>
+				<Main data={{ ticketSubject: 'Test subject' }} />
+			</ThemeProvider>
+		</GlobalContext.Provider>
+	);
+}
+
+describe('Main', () => {
+	afterEach(cleanup);
+
+	describe('workflow to display no search results page', () => {
+		it('displays page correctly when user searches with empty string', () => {
+			const { getAllByDisplayValue, getByDisplayValue, getByText } =
+				renderMain();
+
+			fireEvent.change(getByDisplayValue('Test subject'), {
+				target: { value: '' },
+			});
+			fireEvent.keyDown(getAllByDisplayValue('')[0], { keyCode: 13 });
+
+			getByText('No search results available.');
+			getByText('Create new knowledge.');
+		});
+	});
+
+	describe('workflow to display new article page', () => {
+		it('displays the page correctly when user clicks on the + button', () => {
+			const { getByText } = renderMain();
+
+			fireEvent.click(getByText('+'));
+
+			getByText('Create Knowledge');
+		});
+
+		it('displays the page correctly when user clicks on the "Create new knowledge" link on the no results page', () => {
+			const { getAllByDisplayValue, getByDisplayValue, getByText } =
+				renderMain();
+
+			fireEvent.change(getByDisplayValue('Test subject'), {
+				target: { value: '' },
+			});
+			fireEvent.keyDown(getAllByDisplayValue('')[0], { keyCode: 13 });
+			fireEvent.click(getByText('Create new knowledge.'));
+
+			getByText('Create Knowledge');
+		});
+	});
+});
--- knowledge-capture-app/spec/NoResults.spec.js
+++ knowledge-capture-app/spec/NoResults.spec.js
@@ -3,23 +3,25 @@ import React from 'react';
 
 import NoResults from '../src/javascript/modules/NoResults';
 
+const clickHandler = jest.fn();
+
 describe('Search Filters', () => {
 	afterEach(cleanup);
 
 	it('renders', () => {
-		const { container } = render(<NoResults />);
+		const { container } = render(<NoResults clickHandler={clickHandler} />);
 
 		expect(container).toBeTruthy();
 	});
 
 	it('displays the no results message', () => {
-		const { getByText } = render(<NoResults />);
+		const { getByText } = render(<NoResults clickHandler={clickHandler} />);
 
 		getByText('No search results available.');
 	});
 
 	it('displays link to create new knowledge base article', () => {
-		const { getByText } = render(<NoResults />);
+		const { getByText } = render(<NoResults clickHandler={clickHandler} />);
 
 		getByText('Create new knowledge.');
 	});
