1224edc97af1c03932be89b5a15df35467d6f57d
Rebecca <rhdai10@gmail.com>
2021-09-07 10:57:53 -0700

    ISSUP-4244 hard code KC templates

--- knowledge-capture-app/src/javascript/lib/constants.js
+++ knowledge-capture-app/src/javascript/lib/constants.js
@@ -10,3 +10,11 @@ export const DEFAULT_LOCALE = 'en-US';
 export const NEW_ARTICLE_URL = `${BASE_URL}/knowledge/articles/new`;
 
 export const RESULTS_COUNT = 25;
+
+// hard coded template IDs
+
+export const DXPC_FAST_TRACK_ID =
+	process.env.NODE_ENV === 'production' ? '360041188851' : '360027863431';
+
+export const FAST_TRACK_ID =
+	process.env.NODE_ENV === 'production' ? '360024794552' : '360038699972';
--- knowledge-capture-app/src/javascript/modules/NewArticle.js
+++ knowledge-capture-app/src/javascript/modules/NewArticle.js
@@ -1,28 +1,53 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Button } from '@zendeskgarden/react-buttons';
+import { Anchor, Button } from '@zendeskgarden/react-buttons';
 import { Col, Grid, Row } from '@zendeskgarden/react-grid';
 import i18n from '../lib/i18n';
+import { BASE_URL, DXPC_FAST_TRACK_ID, FAST_TRACK_ID } from '../lib/constants';
 
-export default function NewArticle({ brandId, clickHandler, ticketId }) {
+export default function NewArticle({
+	brandId,
+	clickHandler,
+	locale,
+	ticketId,
+}) {
 	return (
 		<Grid>
 			<PageHeading clickHandler={clickHandler} />
 
-			<ArticleTemplates />
+			<ArticleTemplates
+				brandId={brandId}
+				locale={locale}
+				ticketId={ticketId}
+			/>
 		</Grid>
 	);
 }
 
-const Body = ({ className }) => (
-	<Row className={className}>
-		<Col>{i18n.t('choose a template')}</Col>
-	</Row>
-);
+const Body = ({ brandId, className, locale, ticketId }) => {
+	const generateArticleURL = (templateId) =>
+		`${BASE_URL}/knowledge/articles/new/${locale}?template_id=${templateId}&brand_id=${brandId}&ticket_id=${ticketId}`;
 
-const ArticleTemplates = styled(Body)`
-	padding: ${(p) => p.theme.space.md} 0;
-`;
+	return (
+		<Row className={className}>
+			<Col>
+				{i18n.t('choose a template')}
+
+				<ul>
+					<Template
+						title={i18n.t('dxpc fast track article')}
+						url={generateArticleURL(DXPC_FAST_TRACK_ID)}
+					/>
+
+					<Template
+						title={i18n.t('fast track article')}
+						url={generateArticleURL(FAST_TRACK_ID)}
+					/>
+				</ul>
+			</Col>
+		</Row>
+	);
+};
 
 const CloseButton = ({ className, clickHandler }) => {
 	function handleClick() {
@@ -38,11 +63,6 @@ const CloseButton = ({ className, clickHandler }) => {
 	);
 };
 
-const StyledCloseButton = styled(CloseButton)`
-	color: ${(p) => p.theme.palette.grey[800]};
-	font-weight: ${(p) => p.theme.fontWeights.semibold};
-`;
-
 const Heading = ({ className, clickHandler }) => (
 	<Row alignItems="center">
 		<Col size="10">
@@ -55,8 +75,36 @@ const Heading = ({ className, clickHandler }) => (
 	</Row>
 );
 
+const Template = ({ title, url }) => (
+	<li>
+		<Anchor href={url} target="_blank">
+			{title}
+		</Anchor>
+	</li>
+);
+
+const ArticleTemplates = styled(Body)`
+	padding: ${(p) => p.theme.space.md} 0;
+
+	ul {
+		li {
+			padding: ${(p) => p.theme.space.sm} 0px;
+			word-wrap: break-word;
+
+			~ li {
+				border-top: 1px solid ${(p) => p.theme.palette.grey[300]};
+			}
+		}
+	}
+`;
+
 const PageHeading = styled(Heading)`
 	font-size: ${(p) => p.theme.fontSizes.lg};
 	font-weight: ${(p) => p.theme.fontWeights.semibold};
 	line-height: ${(p) => p.theme.lineHeights.xl};
+`;
+
+const StyledCloseButton = styled(CloseButton)`
+	color: ${(p) => p.theme.palette.grey[800]};
+	font-weight: ${(p) => p.theme.fontWeights.semibold};
 `;
