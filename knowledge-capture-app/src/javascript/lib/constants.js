0c61f13136bd716d12949b55b7133a5ebe257f31
Rebecca <rhdai10@gmail.com>
2021-09-10 11:55:47 -0700

    ISSUP-4245 instantiate a modal when a search result is clicked

--- knowledge-capture-app/src/javascript/lib/constants.js
+++ knowledge-capture-app/src/javascript/lib/constants.js
@@ -11,10 +11,17 @@ export const NEW_ARTICLE_URL = `${BASE_URL}/knowledge/articles/new`;
 
 export const RESULTS_COUNT = 25;
 
+// app location
+
+export const MODAL = 'modal';
+
+// modal dimensions
+export const MAX_RECOMMENDED_HEIGHT = '80vh';
+export const MAX_RECOMMENDED_WIDTH = '80vw';
+
 // hard coded template IDs
 
 export const DXPC_FAST_TRACK_ID =
 	process.env.NODE_ENV === 'production' ? '360041188851' : '360027863431';
-
 export const FAST_TRACK_ID =
 	process.env.NODE_ENV === 'production' ? '360024794552' : '4408854935693';
